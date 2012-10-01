399056eb5743e1745dd7fd8dd1f490aa84914d48
Niko Will <niko.will@avira.com>
2012-10-01 11:36:48 +0200

    complete change of the implemention, not tested yet

--- plugins/russound/__init__.py
+++ plugins/russound/__init__.py
@@ -19,69 +19,112 @@
 #  along with SmartHome.py.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
-import asynchat, asyncore, socket
-import types
 import logging
-import time
+import socket
 import threading
+import struct
+
+import lib.my_asynchat
 
 logger = logging.getLogger('')
 
 REQ_DELIMITER = '\r'
 RESP_DELIMITER = '\r\n'
 
-class Russound(asynchat.async_chat):
+class Russound(lib.my_asynchat.AsynChat):
 
-    lock = threading.Condition()
-    buffer = ''
-    resp = None
-
-    def __init__(self, smarthome):
-        asynchat.async_chat.__init__(self, maü=self.socket_map)
-        logger.info('Starting russound')
+    def __init__(self, smarthome, host, port=9261):
+        lib.my_asynchat.AsynChat.__init__(self, smarthome, host, port)
+        self.terminator = RESP_DELIMITER
         self._sh = smarthome
-        self.all_zones = {}
-        self.controller = {}
-        self.set_terminator(RESP_DELIMITER)
-
-        if 'russound' not in smarthome:
-            logger.warning("No russound configuration found!")
-            return None
-
-        for zone in smarthome['russound']:
-            self.all_zones[zone] = RussoundZone(smarthome, zone)
-            self.controller[zone.controller][zone.zone] = zone
+        self.params = {}
+        smarthome.monitor_connection(self)
 
     def parse_item(self, item):
-        if item.parent in self.all_zones.keys():
-            return self.all_zones[item.parent].update_item
+        if 'rus_path' in item.conf:
+            path = item.conf['rus_path']
+            parts = path.split('.', 2)
+            
+            if len(parts) is not 3:
+                return None
+
+            c = parts[0]
+            z = parts[1]
+            param = parts[2]
+
         else:
-            return None
+            if 'rus_controller' in item.conf:
+                c = item.conf['rus_controller']
+                path = c + '.'
+            else:
+                return None
 
-    def send_set(self, zone, cmd, value):
-        self.send_cmd('SET C[%d].Z[%d].%s="%s"\r' % zone.controller, zone.zone, cmd, value)
+            if 'rus_zone' in item.conf:
+                z = item.conf['rus_zone']
+                path += z + '.'
+            else:
+                return None
 
-    def send_event(self, zone, cmd, value1=None, value2=None):
-        self.send_cmd('EVENT C[%d].Z[%d]!%s %s %s\r' % zone.controller, zone.zone, cmd, value)
+            if 'rus_parameter' in item.conf:
+                param = item.conf['rus_parameter']
+                path += param
+            else:
+                return None
+
+            item.conf['rus_path'] = path
+
+        self.params[path] = {'c': int(c), 'z': int(z), 'param':param, 'item':item}
+        return self.update_item
+
+    def parse_logic(self, logic):
+        pass
+
+    def update_item(self, item, caller=None, source=None):
+        if caller != 'Russound':
+            p = self.params[item.conf['rus_path']]
+            self.update(p.c, p.z, p.param, item())
+            cmd = p.param.tolower()
+
+            if cmd == 'bass':
+                self.send_set(self, cmd, round(item() / (128 / 10)))
+            elif cmd == 'treble':
+                self.send_set(self, cmd, round(item() / (128 / 10)))
+            elif cmd == 'balance':
+                self.send_set(self, cmd, round(item() / (128 / 10)))
+            elif cmd == 'loudness':
+                self.send_set(self, cmd, 'ON' if item() else 'OFF')
+            elif cmd == 'turnonvolume':
+                self.send_set(self, cmd, round(item() / (255 / 50)))
+            elif cmd == 'status':
+                self.send_event(self, 'ZoneOn' if item() else 'ZoneOff')
+            elif cmd == 'partymode':
+                self.send_event(self, 'PartyMode', item().tolower())
+            elif cmd == 'donotdisturb':
+                self.send_event(self, 'DoNotDisturb', 'on' if item() else 'off')
+            elif cmd == 'volume':
+                self.send_event(self, 'KeyPress', 'Volume', round(item() / (255 / 50)))
+            elif cmd == 'currentsource':
+                self.send_event(self, 'SelectSource', item())
+            elif cmd == 'mute':
+                self.send_event(self, 'KeyRelease', 'Mute')
+
+    def send_set(self, c, z, cmd, value):
+        self.send_cmd('SET C[%d].Z[%d].%s="%s"\r' % c, z, cmd, value)
+
+    def send_event(self, c, z, cmd, value1=None, value2=None):
+        self.send_cmd('EVENT C[%d].Z[%d]!%s %s %s\r' % c, z, cmd, value)
         
-    def watch_zone(self, controller, zone, value):
+    def _watch_zone(self, controller, zone, value):
         self.send_cmd('WATCH C[%d].Z[%d] %s\r' % controller, zone, 'ON' if value else 'OFF')
 
-    def watch_system(self, value):
+    def _watch_system(self, value):
         self.send_cmd('WATCH System %s\r' % 'ON' if value else 'OFF')
 
-    def send_cmd(self, cmd):
+    def _send_cmd(self, cmd):
         logger.debug("Request: %s" % cmd)
-        self.lock.acquire()
-        self.resp = None
         self.push(cmd)
-        self.lock.wait(0.5)
-        resp = self.resp
-        self.lock.release()
-        logger.debug("Response: %s" % resp)
-        return resp
 
-    def _handle_response(self, resp):
+    def _parse_response(self, resp):
         if resp[0] == 'S':
             return 
         if resp[0] == 'E':
@@ -95,108 +138,49 @@ class Russound(asynchat.async_chat):
                 resp = resp[10:]
                 cmd = resp.split('=')[0]
                 value = resp.split('"')[1]
-                
-                self.controllers[c][z].item[cmd](value, 'RUSSOUND')
+
+                path = c + '.' + z + '.' + cmd
+                if path in self.params.keys():
+                    self.params[path].item(self._decode(cmd, value), 'Russound')
             elif resp.startswith('System.status'):
                 return
             elif resp[0] == 'S':
                 return
 
+    def _decode(self, cmd, value):
+        cmd = cmd.tolower()
 
-    def _connect(self):
-        print 'connecting...'
-        self.discard_buffers() # clear outgoing buffer
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect( (self.host, self.port) )
-        self.connected=True
-        time.sleep(1)
-
-    def handle_error(self):
-        logger.warning("Error with connection to %s:%s" % (self.host, self.port) )
-        self.close()
-        self.connected = False
-        time.sleep(5)
-
-    def handle_close(self):
-        self.close()
-        self.connected = False
-
-    def collect_incoming_data(self, data):
-        self.buffer += data
+        if cmd == 'bass' or cmd == 'treble' or cmd == 'balance':
+            return round(value * (128 / 10))
+        elif cmd == 'loudness' or cmd == 'status' or cmd == 'mute':
+            return value == 'ON')
+        elif cmd == 'turnonvolume' or cmd == 'volume':
+            return round(value * (255 / 50))
+        elif cmd == 'partymode' or cmd == 'donotdisturb':
+            return value.tolower()
+        elif cmd == 'currentsource':
+            return value 
 
     def found_terminator(self):
-        self.resp = self.buffer
-        if not self.lock.acquire(false): # notify if lock is locked
-            self.lock.notify()
-        self.lock.release()
+        data = self.buffer
         self.buffer = ''
+        self._parse_response(data)
 
     def run(self):
         self.alive = True
-        self.watch_system(True)
+        self._watch_system(True)
 
-        for c in self.controllers:
-            for z in self.controllers[c]:
-                self.watch_zone(c, z, True)
+        for p in self.params:
+            self._watch_zone(p.c, p.z, True)
             
-        while self.alive:
-            if self.connected:
-                asyncore.loop(timeout=1, count=1, map=self.socket_map)
-            else:
-                self._connect()
-
     def stop(self):
         self.alive = False
-        self.watch_system(False)
+        self._watch_system(False)
 
-        for c in self.controllers:
-            for z in self.controllers[c]:
-                self.watch_zone(c, z, False)
+        for p in self.params:
+            self._watch_zone(p.c, p.z, False)
 
-        asyncore.close_all(map=self.socket_map)
-        self.close()
-
-    def parse_logic(self, logic):
-        pass
-
-
-class RussoundZone:
-    def __init__(self, smarthome, russound, item):
-        self._sh = smarthome
-        self.russound = russound
-        self.controller = config['controller']
-        self.zone = config['zone']
-        self._item = item
-
-    def update_item(self, item, caller=None, source=None):
-        if caller != 'RUSSOUND':
-            cmd = item.id().split('.')[-1]
-
-            if cmd == 'bass':
-                self.russound.send_set(self, cmd, round(item() / (128 / 10)))
-            elif cmd == 'treble':
-                self.russound.send_set(self, cmd, round(item() / (128 / 10)))
-            elif cmd == 'balance':
-                self.russound.send_set(self, cmd, round(item() / (128 / 10)))
-            elif cmd == 'loudness':
-                self.russound.send_set(self, cmd, 'ON' if item() else 'OFF')
-            elif cmd == 'turnOnVolume':
-                self.russound.send_set(self, cmd, round(item() / (255 / 50)))
-            elif cmd == 'power':
-                self.russound.send_event(self, 'ZoneOn' if item() else 'ZoneOff')
-            elif cmd == 'partyMode':
-                self.russound.send_event(self, 'PartyMode', item().tolower())
-            elif cmd == 'doNotDisturb':
-                self.russound.send_event(self, 'DoNotDisturb', 'on' if item() else 'off')
-            elif cmd == 'volume':
-                self.russound.send_event(self, 'KeyPress', 'Volume', round(item() / (255 / 50)))
-            elif cmd == 'currentSource':
-                self.russound.send_event(self, 'SelectSource', item())
-            elif cmd == 'mute':
-                if item():
-                    self.russound.send_event(self, 'KeyRelease', 'Mute')
-                else:
-                    self.russound.send_event(self, 'KeyPress', 'Volume', round(self.item.volume() / (255 / 50)))
+        self.handle_close()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
