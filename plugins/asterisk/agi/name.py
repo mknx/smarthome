4fa1d9e975ef937d1a9d574b47f63526203376d2
mknx <mknx@no.thx>
2012-12-16 10:06:37 +0000

    fix reverse lookup of callerid

--- plugins/asterisk/agi/name.py
+++ plugins/asterisk/agi/name.py
@@ -14,15 +14,17 @@ while 1:
 if agi_callerid == 'unknown':
     sys.exit()
 
-lookup = urllib2.urlopen("http://www.dasoertliche.de/Controller?form_name=search_inv&js=no&ph=%s" % agi_callerid, timeout=1)
-lookup = lookup.read()
-exp = re.compile('na: "([a-zA-Z0-9_ ]+)",', re.MULTILINE)
-lookup = exp.search(lookup)
+exp = re.compile('<[^>]*id="name0"[^>]*>([^<]+)<', re.MULTILINE)
+lookup = urllib2.urlopen("http://www.dastelefonbuch.de/?kw={0}&cmd=search".format(agi_callerid), data="User-Agent: Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11", timeout=1)
+lookup = exp.search(lookup.read())
 if lookup != None:
-    sys.stdout.write("SET VARIABLE CALLERID(name) \"%s\"\n" % lookup.group(1))
+    name = lookup.group(1).strip()
+    sys.stdout.write("SET VARIABLE CALLERID(name) \"{0}\"\n".format(name))
     sys.stdout.flush()
     #result = sys.stdin.readline()
-    sys.stdout.write("DATABASE PUT cache %s \"%s\"\n" % (agi_callerid, lookup.group(1)))
+    sys.stdout.write("DATABASE PUT cache {0} \"{1}\"\n".format(agi_callerid, name))
     sys.stdout.flush()
     #result = sys.stdin.readline().strip()
 
+lookup.fp._sock.recv=None
+lookup.close()
