61add30e6bbc4a941b66b91cbdfb6941f19bfc51
mknx <mknx@no.thx>
2012-12-20 10:19:36 +0000

    fixing agi name.py - again

--- plugins/asterisk/agi/name.py
+++ plugins/asterisk/agi/name.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 #
+# cp name.py /usr/share/asterisk/agi-bin/name.py
+#
+
 import sys
 import re
 import urllib
@@ -15,18 +18,18 @@ while 1:
 if agi_callerid == 'unknown':
     sys.exit()
 
-number = urllib.urlencode(agi_callerid)
+number = urllib.quote(agi_callerid)
 exp = re.compile('<[^>]*id="name0"[^>]*>([^<]+)<', re.MULTILINE)
-lookup = urllib2.urlopen("http://www.dastelefonbuch.de/?kw={0}&cmd=search".format(number), data="User-Agent: Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11", timeout=1)
-lookup = exp.search(lookup.read())
-if lookup != None:
-    name = lookup.group(1).strip()
+lookup = urllib2.urlopen("http://www3.dastelefonbuch.de/?kw={0}&cmd=search".format(number), data="User-Agent: Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11", timeout=1)
+name = exp.search(lookup.read())
+if name != None:
+    name = name.group(1).strip()
     sys.stdout.write("SET VARIABLE CALLERID(name) \"{0}\"\n".format(name))
     sys.stdout.flush()
-    line = sys.stdin.read()
+    line = sys.stdin.readline()
     sys.stdout.write("DATABASE PUT cache {0} \"{1}\"\n".format(agi_callerid, name))
     sys.stdout.flush()
-    line = sys.stdin.read()
+    line = sys.stdin.readline()
 
 lookup.fp._sock.recv=None
 lookup.close()
