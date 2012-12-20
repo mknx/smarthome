cd582e9d9a1c6c908e2ecf7bf68f8af7bbbd6313
mknx <mknx@no.thx>
2012-12-20 10:18:40 +0000

    inital mail plugin - IMAP

---
+++ plugins/mail/__init__.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+#########################################################################
+# Copyright 2012 KNX-User-Forum e.V.            http://knx-user-forum.de/
+#########################################################################
+#  This file is part of SmartHome.py.   http://smarthome.sourceforge.net/
+#
+#  SmartHome.py is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SmartHome.py is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
+#########################################################################
+
+import logging
+import imaplib
+import email
+
+logger = logging.getLogger('')
+
+
+class IMAP():
+
+    def __init__(self, smarthome, host, username, password, cycle=300, port=None, ssl=False ):
+        self._sh = smarthome
+        self._ssl = ssl
+        self._host = host
+        self._port = port
+        self._username = username
+        self._password = password
+        self._mail_sub = {}
+        self._mail_to = {}
+        self._mail = False
+        self._sh.scheduler.add('IMAP', self._cycle, cycle=int(cycle))
+
+    def _connect(self):
+        if self._ssl:
+            if self._port != None:
+                imap = imaplib.IMAP4_SSL(self._host, self._port)
+            else:
+                imap = imaplib.IMAP4_SSL(self._host)
+        else:
+            if self._port != None:
+                imap = imaplib.IMAP4(self._host, self._port)
+            else:
+                imap = imaplib.IMAP4(self._host)
+        imap.login(self._username, self._password)
+        return imap
+
+    def _cycle(self):
+        imap = self._connect()
+        rsp, data = imap.select()
+        if rsp != 'OK':
+            logger.warning("IMAP: Could not select mailbox")
+            return
+        rsp, data = imap.uid('search', None, "ALL")
+        if rsp != 'OK':
+            logger.warning("IMAP: Could not search mailbox")
+            return
+        uids = data[0].split()
+        for uid in uids:
+            rsp, data = imap.uid('fetch', uid, '(RFC822)')
+            if rsp != 'OK':
+                logger.warning("IMAP: Could not fetch mail")
+                return
+            mail = email.message_from_string(data[0][1])
+            to = email.utils.parseaddr(mail['To'])[1]
+            fo = email.utils.parseaddr(mail['From'])[1]
+            sub = mail['Subject']
+            if mail['Subject'] in self._mail_sub:
+                logic = self._mail_sub[mail['Subject']]
+            elif to in self._mail_to:
+                logic = self._mail_to[to]
+            elif self._mail:
+                logic = self._mail
+            else:
+                logic = False
+            if logic:
+                logic.trigger('IMAP', fo, mail)
+            else:
+                logger.info("Ingnoring mail. {0} => {1}: {2}".format(fo, to, sub))
+            rsp, data = imap.uid('copy', uid, 'Trash')
+            if rsp == 'OK':
+                typ, data = imap.uid('store', uid, '+FLAGS', '(\Deleted)')
+                logger.debug("Moving mail to trash. {0} => {1}: {2}".format(fo, to, sub))
+        imap.close()
+        imap.logout()
+
+    def run(self):
+        self.alive = True
+
+    def stop(self):
+        self.alive = False
+
+    def parse_item(self, item):
+        pass
+
+    def parse_logic(self, logic):
+        if 'mail_subject' in logic.conf:
+            self._mail_sub[logic.conf['mail_subject']] = logic
+        if 'mail_to' in logic.conf:
+            self._mail_to[logic.conf['mail_to']] = logic
+        if 'mail' in logic.conf:
+            self._mail = logic
+
+    def update_item(self, item, caller=None, source=None):
+        if caller != 'plugin':
+            logger.info("update item: {0}".format(item.id()))
