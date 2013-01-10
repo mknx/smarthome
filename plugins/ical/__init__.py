0cff47ffff4e1b19915ecbada6b850f44adbeae0
mknx <mknx@no.thx>
2013-01-10 21:58:54 +0000

    !new implementation! of the ical plugin to support new keywords

--- plugins/ical/__init__.py
+++ plugins/ical/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-# Copyright 2012 KNX-User-Forum e.V.            http://knx-user-forum.de/
+# Copyright 2013 KNX-User-Forum e.V.            http://knx-user-forum.de/
 #########################################################################
 #  This file is part of SmartHome.py.   http://smarthome.sourceforge.net/
 #
@@ -19,12 +19,13 @@
 #  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
+import logging
 import datetime
+import urllib2
+
 import dateutil.tz
 import dateutil.rrule
 import dateutil.relativedelta
-import urllib2
-import logging
 
 logger = logging.getLogger('')
 
@@ -42,124 +43,166 @@ class iCal():
     def stop(self):
         self.alive = False
 
-    def _parse_date(self, value, dtzinfo, par=''):
+    def parse_item(self, item):
+        pass
+
+    def parse_logic(self, logic):
+        pass
+
+    def update_item(self, item, caller=None, source=None):
+        pass
+
+    def __call__(self, ics, delta=1, offset=0):
+        if ics.startswith('http://'):
+            try:
+                f = urllib2.urlopen(ics)
+                ical = f.read()
+                f.fp._sock.recv = None
+                f.close()
+            except Exception, e:
+                logger.error('Could not open ics file {0}: {1}'.format(ics, e))
+                return {}
+        else:
+            try:
+                f = open(ics, 'r')
+                ical = f.read()
+                f.close()
+            except IOError, e:
+                logger.error('Could not open ics file {0}: {1}'.format(ics, e))
+                return {}
+        now = self._sh.now()
+        offset = offset - 1  # start at 23:59:59 the day before
+        start = now.replace(hour=23, minute=59, second=59, microsecond=0) + datetime.timedelta(days=offset)
+        end = start + datetime.timedelta(days=delta)
+        events = self._parse_ical(ical, ics)
+        ret = {}
+        for event in events:
+            event = events[event]
+            if 'RRULE' in event:
+                for dt in event['RRULE'].between(start, end):
+                    if dt not in event['EXDATES']:
+                        time = dt.time()
+                        date = dt.date()
+                        if date not in ret:
+                            ret[date] = [[time, event['SUMMARY']]]
+                        else:
+                            ret[date].append([time, event['SUMMARY']])
+            else:
+                dt = event['DTSTART']
+                if dt > start and dt < end:
+                    time = dt.time()
+                    date = dt.date()
+                    if date not in ret:
+                        ret[date] = [[time, event['SUMMARY']]]
+                    else:
+                        ret[date].append([time, event['SUMMARY']])
+        return ret
+
+    def _parse_date(self, val, dtzinfo, par=''):
         if par.startswith('TZID='):
             tmp, par, timezone = par.partition('=')
             tzinfo = dateutil.tz.gettz(timezone)
-        else:
-            tzinfo = dtzinfo
-        if 'T' in value:  # ISO datetime
-            value, sep, offset = value.partition('Z')
-            dt = datetime.datetime.strptime(value, "%Y%m%dT%H%M%S")
+        if 'T' in val:  # ISO datetime
+            val, sep, off = val.partition('Z')
+            dt = datetime.datetime.strptime(val, "%Y%m%dT%H%M%S")
         else:  # date
-            y = int(value[0:4])
-            m = int(value[4:6])
-            d = int(value[6:8])
-            dt = datetime.datetime(y, m, d, second=1)
-        dt = dt.replace(tzinfo=tzinfo)
+            y = int(val[0:4])
+            m = int(val[4:6])
+            d = int(val[6:8])
+            dt = datetime.datetime(y, m, d)
+        dt = dt.replace(tzinfo=dtzinfo)
         return dt
 
-    def _parse_event(self, event, dtzinfo, start, end):
-        if 'rrule' not in event:
-            if event['start'] >= start and event['start'] <= end:
-                yield(event['start'])
-        else:
-            kwargs = {}
-            mapping = {'INTERVAL': 'interval', 'WKST': 'wkst', 'COUNT': 'count', }
-            if 'FREQ' in event['rrule']:
-                freq = self.FREQ.index(event['rrule']['FREQ'])
-                del(event['rrule']['FREQ'])
-                if 'DTSTART' not in event['rrule']:
-                    event['rrule']['DTSTART'] = event['start']
-                if 'WKST' in event['rrule']:
-                    if event['rrule']['WKST'] in self.DAYS:
-                        event['rrule']['WKST'] = self.DAYS.index(event['rrule']['WKST'])
-                    else:
-                        event['rrule']['WKST'] = int(event['rrule']['WKST'])
-                if 'BYDAY' in event['rrule']:
-                    day = event['rrule']['BYDAY']
-                    if day.isalpha():
-                        if day in self.DAYS:
-                            day = self.DAYS.index(day)
-                    else:
-                        n = int(day[0:-2])
-                        day = self.DAYS.index(day[-2:])
-                        day = dateutil.rrule.weekday(day, n)
-                    event['rrule']['BYWEEKDAY'] = day
-                    del(event['rrule']['BYDAY'])
-                if 'COUNT' in event['rrule']:
-                    event['rrule']['COUNT'] = int(event['rrule']['COUNT'])
-                if 'INTERVAL' in event['rrule']:
-                    event['rrule']['INTERVAL'] = int(event['rrule']['INTERVAL'])
-                if 'UNTIL' in event['rrule']:
-                    try:
-                        event['rrule']['UNTIL'] = self._parse_date(event['rrule']['UNTIL'], dtzinfo)
-                    except Exception, e:
-                        logger.warning("Problem parsing: {0}: {1}".format(event['summary'], e))
-                for par in event['rrule']:
-                    kwargs[par.lower()] = event['rrule'][par]
-                rr = dateutil.rrule.rrule(freq, **kwargs)
-                for date in rr.between(start, end):
-                    if date not in event['exdate']:
-                        yield(date)
-
-    def __call__(self, file, delta=1, offset=0):
-        event = {}
+    def _parse_ical(self, ical, ics):
         events = {}
         tzinfo = self._sh.tzinfo()
-        now = self._sh.now()
-        start = now.replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=offset)
-        end = start.replace(hour=23, minute=59, second=59) + datetime.timedelta(days=delta)
-        if 'http://' in file:
-            try:
-                ical = urllib2.urlopen(file)
-            except Exception, e:
-                logger.error('Could not open ical file {0}: {1}'.format(file, e))
-                return {}
-        else:
-            try:
-                ical = open(file, 'r')
-            except IOError, e:
-                logger.error('Could not open ical file {0}: {1}'.format(file, e))
-                return {}
-        for line in ical.readlines():
-            line = line.strip()
+        for line in ical.splitlines():
             if line == 'BEGIN:VEVENT':
-                event = {'exdate': []}
+                event = {'EXDATES': []}
             elif line == 'END:VEVENT':
-                for dt in self._parse_event(event, tzinfo, start, end):
-                    time = dt.time()
-                    date = dt.date()
-                    if date not in events:
-                        events[date] = [[time, event['summary']]]
+                if 'UID' not in event:
+                    logger.warning("iCal: problem parsing {0} no UID for event: {1}".format(ics, event))
+                    continue
+                if 'SUMMARY' not in event:
+                    logger.warning("iCal: problem parsing {0} no SUMMARY for UID: {1}".format(ics, event['UID']))
+                    continue
+                if 'DTSTART' not in event:
+                    logger.warning("iCal: problem parsing {0} no DTSTART for UID: {1}".format(ics, event['UID']))
+                    continue
+                if 'DTEND' not in event:
+                    logger.warning("iCal: problem parsing {0} no DTEND for UID: {1}".format(ics, event['UID']))
+                    continue
+                if 'RRULE' in event:
+                    event['RRULE'] = self._parse_rrule(event, tzinfo)
+                if event['UID'] in events:
+                    if 'RECURRENCE-ID' in event:
+                        events[event['UID']]['EXDATES'].append(event['RECURRENCE-ID'])
+                        events[event['UID'] + event['DTSTART'].isoformat()] = event
                     else:
-                        events[date].append([time, event['summary']])
-            elif line.startswith('TZID:'):
-                key, sep, tz = line.partition(':')
-                tzinfo = dateutil.tz.gettz(tz)
+                        logger.warning("iCal: problem parsing {0} duplicate UID: {1}".format(ics, event['UID']))
+                        continue
+                else:
+                    events[event['UID']] = event
             else:
-                key, sep, value = line.partition(':')
+                key, sep, val = line.partition(':')
                 key, sep, par = key.partition(';')
                 key = key.upper()
-                if key == 'DTSTART':
+                if key == 'TZID':
+                    tzinfo = dateutil.tz.gettz(val)
+                elif key in ['UID', 'SUMMARY', 'SEQUENCE', 'RRULE']:
+                    event[key] = val
+                elif key in ['DTSTART', 'DTEND', 'EXDATE', 'RECURRENCE-ID']:
                     try:
-                        event['start'] = self._parse_date(value, tzinfo, par)
+                        date = self._parse_date(val, tzinfo, par)
                     except Exception, e:
-                        logger.warning("Problem parsing: {0}: {1}".format(file, e))
-                elif key == 'DTEND':
-                    try:
-                        event['end'] = self._parse_date(value, tzinfo, par)
-                    except Exception, e:
-                        logger.warning("Problem parsing: {0}: {1}".format(file, e))
-                elif key == 'SUMMARY':
-                    event['summary'] = value
-                elif key == 'RRULE':
-                    event['rrule'] = dict(a.split('=') for a in value.upper().split(';'))
-                elif key == 'EXDATE':
-                    try:
-                        event['exdate'].append(self._parse_date(value, tzinfo, par))
-                    except Exception, e:
-                        logger.warning("Problem parsing: {0}: {1}".format(file, e))
-        ical.fp._sock.recv=None
-        ical.close()
+                        logger.warning("Problem parsing: {0}: {1}".format(ics, e))
+                        continue
+                    if key == 'EXDATE':
+                        event['EXDATES'].append(date)
+                    else:
+                        event[key] = date
         return events
+
+    def _parse_rrule(self, event, tzinfo):
+        rrule = dict(a.split('=') for a in event['RRULE'].upper().split(';'))
+        args = {}
+        if 'FREQ' not in rrule:
+            return
+        freq = self.FREQ.index(rrule['FREQ'])
+        del(rrule['FREQ'])
+        if 'DTSTART' not in rrule:
+            rrule['DTSTART'] = event['DTSTART']
+        if 'WKST' in rrule:
+            if rrule['WKST'] in self.DAYS:
+                rrule['WKST'] = self.DAYS.index(rrule['WKST'])
+            else:
+                rrule['WKST'] = int(rrule['WKST'])
+        if 'BYDAY' in rrule:
+            day = rrule['BYDAY']
+            if day.isalpha():
+                if day in self.DAYS:
+                    day = self.DAYS.index(day)
+            else:
+                n = int(day[0:-2])
+                day = self.DAYS.index(day[-2:])
+                day = dateutil.rrule.weekday(day, n)
+            rrule['BYWEEKDAY'] = day
+            del(rrule['BYDAY'])
+        if 'COUNT' in rrule:
+            rrule['COUNT'] = int(rrule['COUNT'])
+        if 'INTERVAL' in rrule:
+            rrule['INTERVAL'] = int(rrule['INTERVAL'])
+        if 'UNTIL' in rrule:
+            try:
+                rrule['UNTIL'] = self._parse_date(rrule['UNTIL'], tzinfo)
+            except Exception, e:
+                logger.warning("Problem parsing UNTIL: {1} --- {0} ".format(event, e))
+                return
+        for par in rrule:
+            args[par.lower()] = rrule[par]
+        return dateutil.rrule.rrule(freq, **args)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    ical = iCal('smarthome-dummy')
+    print ical('big.ics')
