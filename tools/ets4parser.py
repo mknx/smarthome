07c2a5b62cc091a67d18e8eb25bf6f079a9137c2
marcus <marcus@popp.mx>
2013-06-03 20:17:14 +0000

    fix header typo

--- tools/ets4parser.py
+++ tools/ets4parser.py
@@ -1,4 +1,4 @@
-#!/usr&bin/env python
+#!/usr/bin/env python
 #
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
