e2a7bd93f44b932e7c71662bbe42eb8143a3db91
marcus <marcus@popp.mx>
2013-08-05 20:17:40 +0000

    tweak warning if mail couldn't be deleted

--- plugins/mail/__init__.py
+++ plugins/mail/__init__.py
@@ -96,7 +96,7 @@ class IMAP():
                     typ, data = imap.uid('store', uid, '+FLAGS', '(\Deleted)')
                     logger.debug("Moving mail to trash. {0} => {1}: {2}".format(fo, to, sub))
                 else:
-                    logger.warning("Could not move mail to trash. {0} => {1}: {2}: {3}".format(fo, to, sub, rsp))
+                    logger.warning("Could not move mail to trash. {0} => {1}: {2}".format(fo, to, sub))
             else:
                 logger.info("Ignoring mail. {0} => {1}: {2}".format(fo, to, sub))
         imap.close()
