ba03e8baf31f6ab1605ad0d905b5cae30ed2321e
marcus <marcus@popp.mx>
2013-10-20 19:37:48 +0000

    fix tz for env.location

--- lib/env/location.py
+++ lib/env/location.py
@@ -2,14 +2,14 @@
 # lib/env/location.py
 
 if sh.sun:
-    sh.env.location.sunrise(sh.sun.rise())
-    sh.env.location.sunset(sh.sun.set())
+    sh.env.location.sunrise(sh.sun.rise().astimezone(sh.tzinfo()))
+    sh.env.location.sunset(sh.sun.set().astimezone(sh.tzinfo()))
 
-    sh.env.location.moonrise(sh.moon.rise())
-    sh.env.location.moonset(sh.moon.set())
+    sh.env.location.moonrise(sh.moon.rise().astimezone(sh.tzinfo()))
+    sh.env.location.moonset(sh.moon.set().astimezone(sh.tzinfo()))
     sh.env.location.moonphase(sh.moon.phase())
 
     # setting day and night
-    night = sh.sun.rise(-6).day == sh.sun.set(-6).day
-    sh.env.location.day(not night)
-    sh.env.location.night(night)
+    day = sh.sun.rise(-6).day != sh.sun.set(-6).day
+    sh.env.location.day(day)
+    sh.env.location.night(not day)
