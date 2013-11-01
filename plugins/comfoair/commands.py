6bf5bbc58da81368e5cf06fa27653d4c2015ccca
= <=>
2013-11-01 09:52:35 +0100

    Renamed Zehnder ComfoAir plugin
    Plugin to control a Zehnder ComfoAir
    First Alpha version

---
+++ plugins/comfoair/commands.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+#########################################################################
+# Copyright 2012-2013 KNX-User-Forum e.V.       http://knx-user-forum.de/
+#########################################################################
+#  This file is part of SmartHome.py.
+#
+#  SmartHome.py is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SmartHome.py is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SmartHome.py.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################
+	
+commands = {
+    'KEY_Komforttemp': '00D100',
+    'KEY_Aussentemp': '00D100',
+    'KEY_Zulufttemp': '00D100',
+    'KEY_Ablufttemp': '00D100',
+    'KEY_Fortlufttemp': '00D100',
+    'KEY_EWTtemp': '00D100',
+    'KEY_Vent_Zuluft_Status':'000B00',
+    'KEY_Vent_Abluft_Status':'000B00',
+    'KEY_BypassZustand':'000D00',
+    'KEY_StundenFilter':'00DD00',
+    'KEY_Badschalter':'000300',
+    'KEY_AktuelleStufe':'00CD00',
+    'KEY_Komforttemperatur': '00D301',
+    'KEY_StufeAbwesend':'00990101',
+    'KEY_StufeEins':'00990102',
+    'KEY_StufeZwei':'00990103',
+    'KEY_StufeDrei':'00990104',
+    'KEY_ResetFilter':'00DB0400000001',
+    'KEY_ResetError':'00DB0401000000',
+}
