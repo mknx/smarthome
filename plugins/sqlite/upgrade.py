5583af3471be0562394450bed522eb7a4e791670
marcus <marcus@popp.mx>
2014-01-29 20:40:32 +0000

    sqlite: using external update script

--- plugins/sqlite/upgrade.py
+++ plugins/sqlite/upgrade.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 #
-# history: time, item, avg, vmin, vmax, power
 # num: _end, _start, _item, _avg, _min, _max, _on
 
+import logging
 
-def db2to3(db):
-    db.execute("DROP INDEX IF EXISTS idy;")
-    for item in db.execute("SELECT item FROM history GROUP BY item;").fetchall():
-        end = 0
-        for row in db.execute("SELECT * FROM history WHERE item='{}' ORDER BY time DESC;".format(item[0])).fetchall():
-            time, item, avg, vmin, vmax, power = row
-            if end:
-                insert = (end, time, item, avg, vmin, vmax, power)
-                db.execute("INSERT INTO num VALUES (?,?,?,?,?,?,?);", insert)
-            end = time
-    db.execute("DROP TABLE history;")
+
+logger = logging.getLogger('')
+
+
+def Upgrade(db, version):
+    if version == 1:
+        logger.warning("SQLite: dropping history!")
+        db.execute("DROP TABLE history;")
+        db.execute("DROP INDEX IF EXISTS idx;")
+        db.execute("DROP INDEX IF EXISTS idy;")
+    elif version == 2:
+        # history: time, item, avg, vmin, vmax, power
+        logger.info("SQLite: upgrading database.")
+        db.execute("DROP INDEX IF EXISTS idy;")
+        for item in db.execute("SELECT item FROM history GROUP BY item;").fetchall():
+            end = 0
+            for row in db.execute("SELECT * FROM history WHERE item='{}' ORDER BY time DESC;".format(item[0])).fetchall():
+                time, item, avg, vmin, vmax, power = row
+                if end:
+                    insert = (time, item, end - time, avg, vmin, vmax, power)
+                    db.execute("INSERT INTO num VALUES (?,?,?,?,?,?,?);", insert)
+                end = time
+        db.execute("DROP TABLE history;")
+    elif version == 3:
+        #_start, _item, _dur, _avg, _min, _max, _on
+        pass
     db.execute("VACUUM;")
     db.commit()
