33ce6a4f3e37716e3d677d6c8e356dbb1a18c6d7
pfischi <pfischi@gmx.de>
2014-02-10 20:07:21 +0100

    pydevd removed

--- plugins/sonos/__init__.py
+++ plugins/sonos/__init__.py
@@ -115,7 +115,6 @@ class Sonos():
     def parse_item(self, item):
 
         if 'sonos_recv' in item.conf:
-            #pydevd.settrace('192.168.178.44', port=12000, stdoutToServer=True, stderrToServer=True)
             cmd = self.resolve_cmd(item, 'sonos_recv')
 
             if cmd is None:
