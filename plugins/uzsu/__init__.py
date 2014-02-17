bfcfbb192fdc03e63c861a8fa5af85167af75792
Niko Will <niko.will@avira.com>
2014-02-17 16:07:46 +0100

    pep8 and flake8 code style fixes

--- plugins/uzsu/__init__.py
+++ plugins/uzsu/__init__.py
@@ -20,20 +20,15 @@
 ##########################################################################
 
 import logging
-import datetime
-from datetime import datetime, date, time
+from datetime import datetime
 import sqlite3
-import functools
-import time
 import threading
 
-import dateutil.relativedelta
-from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
-from dateutil.tz import tzutc
-from dateutil.rrule import *
+from dateutil.rrule import rrulestr
 from dateutil import parser
 
 logger = logging.getLogger('')
+
 
 class UZSU():
 
@@ -43,12 +38,16 @@ class UZSU():
     # SQL table definition
     # id        row id to identify the entry
     # item      item for which an entry is for
-    # dt        the date when to set the value or the start of a recurring action
+    # dt        date when to set the value or the start of a recurring action
     # active    False disables a entry temporarily
-    # time      time when to set the value. A %H:%M string or in combination with sunrise/set eg. "17:00" or "17:00<sunset<20:00"
+    # time      time when to set the value. A %H:%M string or in combination
+    #           with sunrise/set eg. "17:00" or "17:00<sunset<20:00"
     # value     the value which will be set to the item
-    # rrule     the recurring rule according RFC 2445 (ftp://ftp.rfc-editor.org/in-notes/rfc2445.txt)
-    _create_db = "CREATE TABLE IF NOT EXISTS uzsu (id INTEGER PRIMARY KEY ASC, item TEXT, dt TIMESTAMP, active BOOLEAN, time TEXT, value TEXT, rrule TEXT);"
+    # rrule     the recurring rule according RFC 2445
+    #           (ftp://ftp.rfc-editor.org/in-notes/rfc2445.txt)
+    _create_db = "CREATE TABLE IF NOT EXISTS uzsu (" \
+        "id INTEGER PRIMARY KEY ASC, item TEXT, " \
+        "dt TIMESTAMP, active BOOLEAN, time TEXT, value TEXT, rrule TEXT);"
 
     def __init__(self, smarthome, path=None):
         logger.info('Init UZSU')
@@ -63,38 +62,49 @@ class UZSU():
         else:
             self.path = path + '/uzsu.db'
         try:
-            self._fdb = sqlite3.connect(self.path, check_same_thread=False, detect_types=sqlite3.PARSE_DECLTYPES)
+            self._fdb = sqlite3.connect(self.path, check_same_thread=False,
+                                        detect_types=sqlite3.PARSE_DECLTYPES)
         except Exception as e:
-            logger.error("UZSU: Could not connect to the database {}: {}".format(self.path, e))
+            logger.error(
+                "UZSU: Couldn't connect to the db {}: {}".format(self.path, e))
             self._fdb_lock.release()
             return
         self.connected = True
         # Check the database integrity
-        integrity = self._fdb.execute("PRAGMA integrity_check(10);").fetchone()[0]
+        integrity = self._fdb.execute(
+            "PRAGMA integrity_check(10);").fetchone()[0]
         if integrity == 'ok':
             logger.debug("UZSU: database integrity ok")
         else:
             logger.error("UZSU: database corrupt. Seek help.")
             self._fdb_lock.release()
             return
+
         # Add the factory for the results.
-        # With this, every row result is stored as a dictionary with the column name as key
+        # With this, every row result is stored as a dictionary with the column
+        # name as key
         def dict_factory(cursor, row):
             d = {}
             for idx, col in enumerate(cursor.description):
                 d[col[0]] = row[idx]
             return d
+
         self._fdb.row_factory = dict_factory
         # Check the version
-        common = self._fdb.execute("SELECT * FROM sqlite_master WHERE name='common' and type='table';").fetchone()
+        common = self._fdb.execute("SELECT * FROM sqlite_master WHERE "
+                                   "name='common' and type='table';")\
+            .fetchone()
         if common is None:
             self._fdb.execute("CREATE TABLE common (version INTEGER);")
-            self._fdb.execute("INSERT INTO common VALUES (:version);", {'version': self._version})
+            self._fdb.execute("INSERT INTO common VALUES (:version);",
+                              {'version': self._version})
             version = self._version
         else:
-            version = int(self._fdb.execute("SELECT version FROM common;").fetchone()['version'])
+            version = int(self._fdb.execute("SELECT version FROM common;")
+                          .fetchone()['version'])
         if version < self._version:
-            self._fdb.execute("UPDATE common SET version=:version;", {'version': self._version})
+            self._fdb.execute("UPDATE common SET version=:version;",
+                              {'version': self._version})
         # Create the database
         self._fdb.execute(self._create_db)
         self._fdb.commit()
@@ -127,11 +137,20 @@ class UZSU():
         return self._items[item]
 
     def add(self, item, dt, value, active=True, time=None, rrule=None):
-        self._items[item].append({'id': None, 'item': item.__str__(), 'dt': dt, 'active': active, 'rrule': rrule, 'time': time, 'value': value})
+        self._items[item].append({
+            'id': None,
+            'item': item.__str__(),
+            'dt': dt,
+            'active': active,
+            'rrule': rrule,
+            'time': time,
+            'value': value
+        })
         if self._db_update(item, self._items[item][-1]):
             self._schedule(item, self._items[item][-1]['id'])
 
-    def update(self, item, id, dt=None, value=None, active=True, time=None, rrule=None):
+    def update(self, item, id, dt=None, value=None, active=True, time=None,
+               rrule=None):
         entry = self._get_entry(item, id)
         if entry:
             entry['active'] = active
@@ -153,10 +172,13 @@ class UZSU():
 
     def _schedule(self, item, id):
         next = self._next_time(item, id)
-        if next: # and next > datetime.now(self._sh.tzinfo()):
+        if next:
             self._sh.scheduler.remove('uzsu_{}_{}'.format(item, id))
             if self._get_entry(item, id)['active']:
-                self._sh.scheduler.add('uzsu_{}_{}'.format(item, id), self._set, value={'item': item, 'id': id}, next=next)
+                self._sh.scheduler.add('uzsu_{}_{}'.format(item, id),
+                                       self._set,
+                                       value={'item': item, 'id': id},
+                                       next=next)
         else:
             self.remove(item, id)
 
@@ -174,7 +196,8 @@ class UZSU():
         now = datetime.now()
         dt = entry['dt']
         timestr = entry['time']
-        rrule = rrulestr(entry['rrule'], dtstart=dt) if entry['rrule'] else None
+        rrule = rrulestr(entry['rrule'], dtstart=dt) if entry[
+            'rrule'] else None
         try:
             if timestr and rrule:
                 dt = now
@@ -183,20 +206,30 @@ class UZSU():
                     if dt is None:
                         return
                     if 'sun' in timestr:
-                        next = self._sun(datetime.combine(dt.date(), datetime.min.time()).replace(tzinfo=self._sh.tzinfo()), timestr)
+                        next = self._sun(datetime.combine(
+                            dt.date(), datetime.min.time()).replace(
+                            tzinfo=self._sh.tzinfo()), timestr)
                     else:
-                        next = datetime.combine(dt.date(), parser.parse(timestr.strip()).time()).replace(tzinfo=self._sh.tzinfo())
-                    if next and next.date() == dt.date() and next > datetime.now(self._sh.tzinfo()):
+                        next = datetime.combine(
+                            dt.date(), parser.parse(timestr.strip()).time())\
+                            .replace(tzinfo=self._sh.tzinfo())
+                    if next and next.date() == dt.date()\
+                            and next > datetime.now(self._sh.tzinfo()):
                         return next
             elif rrule:
                 next = rrule.after(now)
                 return next.replace(tzinfo=self._sh.tzinfo()) if next else None
             elif timestr:
                 if 'sun' in timestr:
-                    next = self.sun(datetime.combine(dt.date(), datetime.min.time()).replace(tzinfo=self._sh.tzinfo()), timestr)
+                    next = self.sun(
+                        datetime.combine(dt.date(), datetime.min.time())
+                        .replace(tzinfo=self._sh.tzinfo()), timestr)
                 else:
-                    next = datetime.combine(dt.date(), parser.parse(timestr.strip()).time()).replace(tzinfo=self._sh.tzinfo())
-                if next and next.date() == dt.date() and next > datetime.now(self._sh.tzinfo()):
+                    next = datetime.combine(
+                        dt.date(), parser.parse(timestr.strip()).time())\
+                        .replace(tzinfo=self._sh.tzinfo())
+                if next and next.date() == dt.date()\
+                        and next > datetime.now(self._sh.tzinfo()):
                     return next
             elif dt > now:
                 return dt.replace(tzinfo=self._sh.tzinfo())
@@ -205,7 +238,9 @@ class UZSU():
 
     def _sun(self, dt, tstr):
         if not self._sh.sun:  # no sun object created
-            logger.warning('No latitude/longitude specified. You could not use sunrise/sunset as UZSU entry.')
+            logger.warning(
+                'No latitude/longitude specified. '
+                'You could not use sunrise/sunset as UZSU entry.')
             return
         # find min/max times
         tabs = tstr.split('<')
@@ -227,7 +262,8 @@ class UZSU():
             cron = tabs[1].strip()
             smax = tabs[2].strip()
         else:
-            logger.error('Wrong syntax: {0}. Should be [H:M<](sunrise|sunset)[+|-][offset][<H:M]'.format(tstr))
+            logger.error('Wrong syntax: {0}. Should be [H:M<](sunrise|sunset)'
+                         '[+|-][offset][<H:M]'.format(tstr))
             return
 
         doff = 0  # degree offset
@@ -251,24 +287,37 @@ class UZSU():
         elif cron.startswith('sunset'):
             next_time = self._sh.sun.set(doff, moff, dt=dt)
         else:
-            logger.error('Wrong syntax: {0}. Should be [H:M<](sunrise|sunset)[+|-][offset][<H:M]'.format(tstr))
+            logger.error('Wrong syntax: {0}. Should be [H:M<](sunrise|sunset)'
+                         '[+|-][offset][<H:M]'.format(tstr))
             return
 
         if smin is not None:
             h, sep, m = smin.partition(':')
             try:
-                dmin = next_time.replace(hour=int(h), minute=int(m), second=0, tzinfo=self._sh.tzinfo())
+                dmin = next_time.replace(
+                    hour=int(h),
+                    minute=int(m),
+                    second=0,
+                    tzinfo=self._sh.tzinfo())
             except Exception:
-                logger.error('Wrong syntax: {0}. Should be [H:M<](sunrise|sunset)[+|-][offset][<H:M]'.format(tstr))
+                logger.error('Wrong syntax: {0}. Should be [H:M<]'
+                             '(sunrise|sunset)[+|-][offset][<H:M]'
+                             .format(tstr))
                 return
             if dmin > next_time:
                 next_time = dmin
         if smax is not None:
             h, sep, m = smax.partition(':')
             try:
-                dmax = next_time.replace(hour=int(h), minute=int(m), second=0, tzinfo=self._sh.tzinfo())
+                dmax = next_time.replace(
+                    hour=int(h),
+                    minute=int(m),
+                    second=0,
+                    tzinfo=self._sh.tzinfo())
             except Exception:
-                logger.error('Wrong syntax: {0}. Should be [H:M<](sunrise|sunset)[+|-][offset][<H:M]'.format(tstr))
+                logger.error('Wrong syntax: {0}. Should be [H:M<]'
+                             '(sunrise|sunset)[+|-][offset][<H:M]'
+                             .format(tstr))
                 return
             if dmax < next_time:
                 next_time = dmax
@@ -276,39 +325,50 @@ class UZSU():
 
     def _db_fetchall(self, item):
         if not self._fdb_lock.acquire(timeout=2):
-            logger.error("UZSU: init item {} failed - db is locked".format(item))
+            logger.error(
+                "UZSU: init item {} failed - db is locked".format(item))
             return
         if not self.connected:
-            logger.error("UZSU: init item {} failed - db is disconnected".format(item))
+            logger.error(
+                "UZSU: init item {} failed - db is disconnected".format(item))
             self._fdb_lock.release()
             return
         try:
-            return self._fdb.execute("SELECT * FROM uzsu WHERE item='{0}' ORDER BY id".format(item)).fetchall()
+            return self._fdb.execute(
+                "SELECT * FROM uzsu WHERE item='{0}' ORDER BY id"
+                .format(item)).fetchall()
         except Exception as e:
-            logger.error("UZSU: init item {} failed - db query failed".format(item))
+            logger.error("UZSU: init item {} failed - "
+                         "db query failed - {}".format(item, e))
         finally:
             self._fdb_lock.release()
 
     def _db_update(self, item, entry):
         if not self._fdb_lock.acquire(timeout=10):
-            logger.error("UZSU: insert or replace item {} failed - lock timeout".format(entry['item']))
+            logger.error("UZSU: insert or replace item {} failed - "
+                         "lock timeout".format(entry['item']))
             return
         try:
             columns = ', '.join(entry.keys())
             placeholders = ', '.join('?' * len(entry))
-            sql = 'INSERT OR REPLACE INTO uzsu ({}) VALUES ({})'.format(columns, placeholders)
-            entry['id'] = self._fdb.execute(sql, tuple(entry.values())).lastrowid
+            sql = 'INSERT OR REPLACE INTO uzsu ({}) VALUES ({})'.format(
+                columns, placeholders)
+            entry['id'] = self._fdb.execute(
+                sql, tuple(entry.values())).lastrowid
             self._fdb.commit()
-            logger.debug("UZSU: dumped item {} success - values: {}".format(item, entry))
+            logger.debug("UZSU: dumped item {} success - "
+                         "values: {}".format(item, entry))
             return True
         except Exception as e:
-            logger.warning("UZSU: insert or replace item {} failed: {}".format(item, e))
+            logger.warning("UZSU: insert or replace item {} failed: "
+                           "{}".format(item, e))
         finally:
             self._fdb_lock.release()
 
     def _db_remove(self, item, id):
         if not self._fdb_lock.acquire(timeout=10):
-            logger.error("UZSU: remove item {} failed - lock timeout".format(item))
+            logger.error("UZSU: remove item {} failed - "
+                         "lock timeout".format(item))
             return
         try:
             self._fdb.execute("DELETE FROM uzsu WHERE id = ?;", (id,))
