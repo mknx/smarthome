099437abe5b65399be199d19f0b8bed0c79793f1
marcus <marcus@popp.mx>
2014-03-06 20:36:21 +0000

    fritzbox: rewritten

--- plugins/fritzbox/README.md
+++ plugins/fritzbox/README.md
@@ -2,7 +2,9 @@
 
 # Requirements
 This plugin has no requirements or dependencies.
-At the moment only fritzbox firmware versions before 5.50 are supported.
+At the moment only fritzbox firmware versions after 5.50 are supported.
+
+I have tested it with my FritzBox 7390 and FritzOS 06.03.
 
 # Configuration
 
@@ -11,48 +13,106 @@ At the moment only fritzbox firmware versions before 5.50 are supported.
 [fritzbox]
     class_name = FritzBox
     class_path = plugins.fritzbox
-    host = fritz.box
-    password = blub
+    username = secret
+    password = secret
+#   host = fritz.box
+#   cycle = 300
 </pre>
 
 ### Attributes
+  * `username`/password`: required login information
   * `host`: specifies the hostname or ip address of the FritzBox.
-  * `password`: the password of the FritzBox web interface.
+  * `cycle`: timeperiod between two update cycles. Default is 300 seconds.
 
 ## items.conf
 
 ### fritzbox
-This attribute defines supported functions of the plugin. The function is executed, when the item is set to a bool `true` value.
-Functions supported in the plugin:
- * `call <<from>> <<to>>`: The FritzBox will initiate a call from the number (outgoing line) defined with `from` to a number defined with `to`.
+This attribute defines supported functions of the plugin. See the the example for the supported values and options.
 
-### fritzbox:<<telcfg>>
-This attributes represents direct access to the FritzBox webinterface. Each attribute which starts with `fritzbox:` is taken to create a dictionary, which is sent to the FritzBox. Here you can use every command which is available for the telcfg interface (just replace the `telcfg:` with `fritzbox:`). A list of known commands is described here: http://www.wehavemorefun.de/fritzbox/Telcfg
-
-Example item:
-
+### Example:
 <pre>
-[fb]
-    [[call1]]
-        type=bool
-        fritzbox=call **610 **611
-    [[call2]]
-        type=bool
-        fritzbox:settings/UseClickToDial = '1'
-        fritzbox:command/Dial = '**610'
-        fritzbox:settings/DialPort = '**611'
+[example]
+    [[fritzbox]]
+        [[[ip]]]
+            type = str
+            fritzbox = external_ip
+        [[[connected]]]
+            type = bool
+            fritzbox = connected
+        [[[packets_sent]]]
+            type = num
+            fritzbox = packets_sent
+        [[[packets_received]]]
+            type = num
+            fritzbox = packets_received
+        [[[bytes_sent]]]
+            type = num
+            fritzbox = bytes_sent
+        [[[bytes_received]]]
+            type = num
+            fritzbox = bytes_received
+        [[[tam]]]  # telephone answering machine
+            type = bool
+            fritzbox = tam # read only!
+            fb_tam = 0
+        [[[tam2]]]  # 2nd telephone answering machine
+            type = bool
+            fritzbox = tam # read only!
+            fb_tam = 1
+        [[[wlan]]]
+            type = bool
+            fritzbox = wlan
+        [[[wlan_2]]]
+            type = bool
+            fritzbox = wlan
+            fb_wlan = 2  # 5 GHz
+        [[[link]]]
+            type = bool
+            fritzbox = link
+        [[[host]]]
+            type = bool
+            fritzbox = host
+            fb_mac = XX:XX:XX:XX:XX:XX 
+    [[fbswitch]]
+        type = bool
+        fritzbox = switch
+        fb_ain = 081111111111
+        [[[energy]]]
+            type = num
+            fritzbox = energy  # Wh
+        [[[power]]]
+            type = num
+            fritzbox = power  # mW
+    [[fbswitch2]]
+        type = num
+        fritzbox = power
+        fb_ain = 081111111111
+        eval = value / 1000  # convert from mW to W
 </pre>
 
-Both `call1` and `call2` will have the same effect. The first uses the implemented call function. The later uses the telcfg commands which are used internally in the call function. With the second option you can control almost anything which can be controlled via the web interface of your FritzBox.
-
 ## logic.conf
+If you specify `fritzbox = callmonitor` your logic will be called at every call event of your FritzBox. Have a look at the trigger['value'] for information about the event.
+You have to enable the monitor of your FritzBox by calling `#96*5*` from your phone. You could disable it with `#96*4*`.
 
-Currently there is no logic configuration for this plugin.
+<pre>
+[Callmonitor]
+    filename = callmonitor.py
+    fritzbox = callmonitor
+</pre>
 
 # Functions
 
 ## call(from, to)
-This function calls a specified number with the specified caller.
+This function calls a specified number from the specified caller. If your calling external numbers, you could speed up the dial process by appending a `#` to the to-number.
 <pre>
 sh.fritzbox.call('**610', '**611')
 </pre>
+
+## calllist()
+This function returns a list of all calls.
+
+## reboot()
+This function reboots the FritzBox.
+
+## reconnect()
+This function reconnect the upstream connection.
--- plugins/fritzbox/__init__.py
+++ plugins/fritzbox/__init__.py
@@ -1,164 +1,313 @@
 #!/usr/bin/env python3
-# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #
-# Copyright 2012-2013 KNX-User-Forum e.V.       http://knx-user-forum.de/
-#
-#  This file is part of SmartHome.py.    http://mknx.github.io/smarthome/
-#
-#  SmartHome.py is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  SmartHome.py is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
-#
+#########################################################################
+#  Copyright 2014 Marcus Popp                              marcus@popp.mx
+#########################################################################
+#  Free for non-commercial use
+#########################################################################
 
-import sys
+import datetime
 import logging
-import http.client
-import urllib.request
-import urllib.parse
-import urllib.error
 import hashlib
-import re
+import urllib
+import xml.etree.cElementTree
+
+import lib.connection
+import lib.www
 
 logger = logging.getLogger('')
 
+services = {'WANIPConnection': ('/upnp/control/wanipconnection1', 'urn:dslforum-org:service:WANIPConnection:1'),
+            'WLANConfiguration1': ('/upnp/control/wlanconfig1', 'urn:dslforum-org:service:WLANConfiguration:1'),
+            'WLANConfiguration2': ('/upnp/control/wlanconfig2', 'urn:dslforum-org:service:WLANConfiguration:2'),
+            'WLANConfiguration3': ('/upnp/control/wlanconfig3', 'urn:dslforum-org:service:WLANConfiguration:3'),
+            'X_AVM-DE_OnTel': ('/upnp/control/x_contact', 'urn:dslforum-org:service:X_AVM-DE_OnTel:1'),
+            'DeviceConfig': ('/upnp/control/deviceconfig', 'urn:dslforum-org:service:DeviceConfig:1'),
+            'TAM': ('/upnp/control/x_tam', 'urn:dslforum-org:service:X_AVM-DE_TAM:1'),
+            'Hosts': ('/upnp/control/hosts', 'urn:dslforum-org:service:Hosts:1'),
+            'VoIP': ('/upnp/control/x_voip', 'urn:dslforum-org:service:X_VoIP:1'),
+            'WANCommonInterfaceConfig': ('/upnp/control/wancommonifconfig1', 'urn:dslforum-org:service:WANCommonInterfaceConfig:1')}
 
-class fbex(Exception):
-    pass
+values = {'external_ip': ('WANIPConnection', 'GetExternalIPAddress', 'NewExternalIPAddress', None),
+          'connected': ('WANIPConnection', 'GetStatusInfo', 'NewConnectionStatus', 'Connected'),
+          'packets_sent': ('WANCommonInterfaceConfig', 'GetTotalPacketsSent', 'NewTotalPacketsSent', None),
+          'packets_received': ('WANCommonInterfaceConfig', 'GetTotalPacketsReceived', 'NewTotalPacketsReceived', None),
+          'bytes_sent': ('WANCommonInterfaceConfig', 'GetTotalBytesSent', 'NewTotalBytesSent', None),
+          'bytes_received': ('WANCommonInterfaceConfig', 'GetTotalBytesReceived', 'NewTotalBytesReceived', None),
+          'tam': ('TAM', 'GetInfo', 'NewEnable', '1'),
+          'host': ('Hosts', 'GetSpecificHostEntry', 'NewActive', '1'),
+          'wlan': ('WLANConfiguration1', 'GetInfo', 'NewEnable', '1'),
+          'wlan_1': ('WLANConfiguration1', 'GetInfo', 'NewEnable', '1'),
+          'wlan_2': ('WLANConfiguration2', 'GetInfo', 'NewEnable', '1'),
+          'wlan_3': ('WLANConfiguration3', 'GetInfo', 'NewEnable', '1'),
+          'link': ('WANCommonInterfaceConfig', 'GetCommonLinkPropertiesl', 'NewPhysicalLinkStatus', None)
+          }
+#         'calls': ('VoIP', 'X_AVM-DE_DialGetConfig', 'NewX_AVM-DE_PhoneName', None),
+#         'phonebook': ('X_AVM-DE_OnTel', 'GetCallList', '', None)
+#         'port': ('VoIP', 'X_AVM-DE_GetPhonePort', 'NewX_AVM-DE_PhoneName', None),
+#         'clients': ('VoIP', 'X_AVM-DE_GetClients', 'NewX_AVM-DE_ClientList', None),
 
+commands = {'reconnect': ('WANIPConnection', 'ForceTermination'),
+            'reboot': ('DeviceConfig', 'Reboot', None),
+            'tam': ('TAM', 'SetEnable', 'NewEnable'),
+            'setport': ('VoIP', 'X_AVM-DE_DialSetConfig', 'NewX_AVM-DE_PhoneName'),
+            'call': ('VoIP', 'X_AVM-DE_DialNumber', 'NewX_AVM-DE_PhoneNumber'),
+            'wlan': ('WLANConfiguration1', 'SetEnable', 'NewEnable'),
+            'wlan_1': ('WLANConfiguration1', 'SetEnable', 'NewEnable'),
+            'wlan_2': ('WLANConfiguration2', 'SetEnable', 'NewEnable'),
+            'wlan_3': ('WLANConfiguration3', 'SetEnable', 'NewEnable')}
+#           'hangup': ('VoIP', 'X_AVM-DE_Hangup', None),
 
-class FritzBoxBase():
-
-    def __init__(self, host='fritz.box', password=None):
-        self._host = host
-        self._password = password
-        self._sid = 0
-
-    def _login(self):
-        params = urllib.parse.urlencode(
-            {'getpage': '../html/login_sid.xml', 'sid': self._sid})
-        headers = {"Content-type":
-                   "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        con = http.client.HTTPConnection(self._host)
-        con.request("POST", "/cgi-bin/webcm", params, headers)
-        resp = con.getresponse()
-        con.close()
-        if resp.status != 200:
-            raise fbex("no connection to fritzbox.")
-        data = resp.read().decode('iso-8859-1')
-        logger.debug("data = {0}".format(data))
-        sid = re.search('<SID>(.*?)</SID>', data).group(1)
-        logger.debug("sid = {0}".format(sid))
-        if sid == '0000000000000000':
-            logger.debug("invalid sid, starting challenge/response")
-            challenge = re.search('<Challenge>(.*?)</Challenge>', data).group(1)
-            challenge_resp = challenge + '-' + self._password
-            m = hashlib.md5()
-            m.update(challenge_resp)
-            challenge_resp = challenge + '-' + m.hexdigest().lower()
-            params = urllib.parse.urlencode(
-                {'login:command/response': challenge_resp, 'getpage': '../html/login_sid.xml'})
-            con = http.client.HTTPConnection(self._host)
-            con.request("POST", "/cgi-bin/webcm", params, headers)
-            resp = con.getresponse()
-            con.close()
-            if resp.status != 200:
-                raise fbex("challenge/response failed")
-            data = resp.read().decode('iso-8859-1')
-            self._sid = re.search('<SID>(.*?)</SID>', data).group(1)
-            logger.debug('session id = {0}'.format(self._sid))
-
-    def execute(self, cmd_dict, return_resp=False):
-        logger.debug("execute command: {0}".format(cmd_dict))
-        self._login()
-        cmd_dict['getpage'] = '../html/login_sid.xml'
-        cmd_dict['sid'] = self._sid
-        params = urllib.parse.urlencode(cmd_dict)
-        headers = {"Content-type":
-                   "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        con = http.client.HTTPConnection(self._host)
-        con.request("POST", "/cgi-bin/webcm", params, headers)
-        resp = con.getresponse()
-        con.close()
-        if resp.status != 200:
-            raise fbex("execution failed: {0}".format(cmd_dict))
-        if return_resp:
-            return resp
-
-    def call(self, call_from, call_to):
-        logger.debug(
-            "initiate call from {0} to {1}".format(call_from, call_to))
-        self.execute({
-            'telcfg:settings/UseClickToDial': 1,
-            'telcfg:command/Dial': call_to,
-            'telcfg:settings/DialPort': call_from
-        })
+# http://www.avm.de/de/News/artikel/schnittstellen_und_entwicklungen.php
+# http://www.wehavemorefun.de/fritzbox/Telcfg
 
 
-class FritzBox(FritzBoxBase):
+class CallMonitor(lib.connection.Client):
+    # Ausgehende Anrufe: datum;CALL;ConnectionID;Nebenstelle;GenutzteNummer;AngerufeneNummer;
+    # Eingehende Anrufe: datum;RING;ConnectionID;Anrufer-Nr;Angerufene-Nummer;
+    # Zustandegekommene Verbindung: datum;CONNECT;ConnectionID;Nebenstelle;Nummer;
+    # Ende der Verbindung: datum;DISCONNECT;ConnectionID;dauerInSekunden;
+    # enable with "#96*5*"
+    # disable with "#96*4*"
 
-    def __init__(self, smarthome, host='fritz.box', password=None):
-        FritzBoxBase.__init__(self, host, password)
+    port = 1012
+
+    def __init__(self, logics, host='fritz.box'):
+        lib.connection.Client.__init__(self, host, self.port, monitor=True)
+        self._logics = logics
+
+    def found_terminator(self, data):
+        value = data.decode().split(';')
+        source = ''
+        destination = ''
+        if value[1] == 'CALL':
+            source = value[3]
+            destination = value[5]
+        elif value[1] == 'RING':
+            source = value[3]
+            destination = value[4]
+        for logic in self._logics:
+            logic.trigger('FritzBox', source, value, destination)
+
+
+class FritzBox(lib.www.Client):
+
+    _body = """
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+    s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body>
+<u:{0} xmlns:u="{1}">{2}</u:{0}>
+</s:Body>
+</s:Envelope>"""
+
+    def __init__(self, smarthome, username, password, host='fritz.box', cycle=300):
+        self._sid = False
         self._sh = smarthome
+        self._fritzbox = host
+        self._cycle = int(cycle)
+        self._username = username
+        self._password = password
+        self._items = []
+        self._ahas = []
+        self._callmonitor_logics = []
 
     def run(self):
         self.alive = True
+        self._sh.scheduler.add('fb-cycle', self._update_cycle, prio=5, cycle=self._cycle, offset=2)
+        content = self._aha_command('getswitchlist')
+        if content:
+            logger.info("FritzBox: found the following AIN: {}".format(content))
+        if self._callmonitor_logics != []:
+            self.__callmonitor = CallMonitor(self._callmonitor_logics, self._fritzbox)
 
     def stop(self):
         self.alive = False
 
     def parse_item(self, item):
         if 'fritzbox' in item.conf:
-            return self.update_item
-        fb = {}
-        for attr in item.conf:
-            if attr.startswith('fritzbox'):
-                fb['telcfg{0}'.format(attr[8:])] = item.conf[attr]
-        if len(fb) > 0:
-            item.conf['fritzbox'] = fb
-            print(fb)
-            return self.update_item
+            value = item.conf['fritzbox']
+            if value in values:
+                if value == 'wlan':
+                    if 'fb_wlan' in item.conf:
+                        item.conf['fritzbox'] = "wlan_{}".format(item.conf['fb_wlan'])
+                elif value == 'tam':
+                    if not 'fb_tam' in item.conf:
+                        item.conf['fb_tam'] = 0
+                elif value == 'host':
+                    if not 'fb_mac' in item.conf:
+                        logger.warning("FritzBox: please specify fb_mac for {}".format(item.id()))
+                        return
+                self._items.append(item)
+            if value in commands:
+                return self.update_item
+            if value in ['switch', 'power', 'energy']:
+                if 'fb_ain' in item.conf:
+                    ain = item.conf['fb_ain']
+                elif 'fb_ain' in item.return_parent().conf:
+                    ain = item.return_parent().conf['fb_ain']
+                else:
+                    logger.warning("FritzBox: please specify fb_ain for {}".format(item.id()))
+                    return
+                item.conf['fb_ain'] = ain.replace(' ', '').strip()
+                self._ahas.append(item)
+                if value == 'switch':
+                    return self.update_aha
+
+    def parse_logic(self, logic):
+        if 'fritzbox' in logic.conf:
+            if logic.conf['fritzbox'] == 'callmonitor':
+                self._callmonitor_logics.append(logic)
+
+    def update_aha(self, item, caller=None, source=None, dest=None):
+        if caller != 'FritzBox':
+            if item():
+                command = 'setswitchon'
+            else:
+                command = 'setswitchoff'
+            self._aha_command(command, item.conf['fb_ain'])
 
     def update_item(self, item, caller=None, source=None, dest=None):
-        if caller != 'Fritzbox' and item():
-            attr = item.conf['fritzbox']
-            if isinstance(attr, dict):
-                self.execute(attr)
+        if caller != 'FritzBox':
+            command = item.conf['fritzbox']
+            if command.startswith('wlan'):
+                self._set(command, int(item()))
+            elif command == 'tam':
+                self._set(command, str(item()).lower(), index=item.conf['fb_tam'])
+            else:
+                self._set(command)
+
+    # Plugin specific public methods
+
+    def call(self, call_from, call_to):
+        self._set('setport', call_from)
+        self._set('call', "{}".format(call_to))
+
+    def calllist(self):
+        content = self.fetch_url("https://{}:49443/calllist.lua?sid={}".format(self._fritzbox, self._get_sid()))
+        if content:
+            entries = []
+            tree = xml.etree.cElementTree.fromstring(content.decode())
+            for call in tree.findall('Call'):
+                entry = {}
+                for element in call:
+                    entry[element.tag] = element.text
+                entry['Date'] = datetime.datetime.strptime(entry['Date'], '%d.%m.%y %H:%M')
+                entries.append(entry)
+            return entries
+
+    def reboot(self):
+        self._set('reboot')
+
+    def reconnect(self):
+        self._set('reconnect')
+
+    # Plugin specific private methods
+    def _aha_command(self, command, ain=None):
+        # http://www.avm.de/de/Extern/files/session_id/AHA-HTTP-Interface.pdf
+        url = "http://{}/webservices/homeautoswitch.lua?switchcmd={}&sid={}".format(self._fritzbox, command, self._get_sid())
+        if ain is not None:
+            url += "&ain={}".format(ain)
+        content = self.fetch_url(url)
+        return(content.decode().strip())
+
+    def _get(self, variable, value='', index=None):
+        if variable in values:
+            service, action, element, check = values[variable]
+            url, service = services[service]
+        else:
+            return
+        url = "https://{}:49443{}".format(self._fritzbox, url)
+        headers = {'CONTENT-TYPE': 'text/xml; charset="utf-8"'}
+        headers['SOAPACTION'] = "{}#{}".format(service, action)
+        if index is not None:
+            value += "<{0}>{1}</{0}>".format('NewIndex', index)
+        body = self._body.format(action, service, value)
+        content = self.fetch_url(url, auth='digest', headers=headers, body=body, username=self._username, password=self._password, method='POST')
+        if content and element:
+            tree = xml.etree.cElementTree.fromstring(content.decode())
+            element = tree.find('.//{}'.format(element))
+            if element is None:
                 return
-            if isinstance(attr, str):
-                match = re.search(
-                    r'\s*call\s(?P<from>[^\s]+)\s+(?P<to>[^\s]+)\s*', attr)
-                if match:
-                    self.call(match.group('from'), match.group('to'))
-                    return
-            logger.debug(
-                "fritzbox attribute value {0} on item {1} not recognized".format(attr, item))
+            if check is None:
+                try:
+                    return int(element.text)
+                except:
+                    return element.text
 
+            else:
+                return element.text == check
 
-def main():
-    if len(sys.argv) != 4:
-        print("usage: {0} password from to".format(sys.argv[0]))
-        return 1
+    def _get_sid(self):
+        if self._sid:
+            return self._sid
+        content = self.fetch_url("http://{}/login_sid.lua".format(self._fritzbox))
+        if content:
+            tree = xml.etree.cElementTree.fromstring(content.decode())
+            challenge = tree.find('.//Challenge').text
+            response = hashlib.md5("{}-{}".format(challenge, self._password).encode('utf-16le')).hexdigest()
+            url = "http://{}/login_sid.lua?username={}&response={}-{}".format(self._fritzbox, self._username, challenge, response)
+            content = self.fetch_url(url)
+            tree = xml.etree.cElementTree.fromstring(content.decode())
+            sid = tree.find('.//SID').text
+            if sid != '0000000000000000':
+                self._sid = sid
+                return sid
 
-    handler = logging.StreamHandler(sys.stdout)
-    frm = logging.Formatter(
-        "%(asctime)s %(levelname)s: %(message)s", "%d.%m.%Y %H:%M:%S")
-    handler.setFormatter(frm)
+    def _set(self, variable, value='', index=None):
+        if variable in commands:
+            service, action, element = commands[variable]
+            url, service = services[service]
+        else:
+            return
+        url = "https://{}:49443{}".format(self._fritzbox, url)
+        headers = {'CONTENT-TYPE': 'text/xml; charset="utf-8"'}
+        headers['SOAPACTION'] = "{}#{}".format(service, action)
+        if element:
+            value = "<{0}>{1}</{0}>".format(element, value)
+        if index is not None:
+            value = "<{0}>{1}</{0}>{2}".format('NewIndex', index, value)  # für tam notwendig
+        body = self._body.format(action, service, value)
+        self.fetch_url(url, auth='digest', headers=headers, body=body, username=self._username, password=self._password, method='POST')
 
-    logger = logging.getLogger()
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
+    def _update_cycle(self):
+#       start = time.time()
+        for item in self._items:
+            if not self.alive:
+                return
+            value = item.conf['fritzbox']
+            if value == 'host':
+                value = self._get('host', "<NewMACAddress>{}</NewMACAddress>".format(item.conf['fb_mac']))
+            elif value == 'tam':
+                value = self._get(value, index=item.conf['fb_tam'])
+            else:
+                value = self._get(value)
+            if value is not None:
+                item(value, 'FritzBox')
+        for item in self._ahas:
+            if not self.alive:
+                return
+            value = item.conf['fritzbox']
+            if value == 'power':
+                command = 'getswitchpower'
+            elif value == 'energy':
+                command = 'getswitchenergy'
+            else:
+                command = 'getswitchstate'
+            value = self._aha_command(command, item.conf['fb_ain'])
+            if value is not None:
+                item(int(value), 'FritzBox')
+#       cycletime = time.time() - start
+#       logger.debug("cycle takes {0} seconds".format(cycletime))
 
-    fb = FritzBoxBase(password=sys.argv[1])
-    fb.call(sys.argv[2], sys.argv[3])
+    def webcm(self, command={}):
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+        command['sid'] = self._get_sid()
+        command['getpage'] = '../html/login_sid.xml'
+        body = urllib.parse.urlencode(command)
+        url = "http://{}/cgi-bin/webcm".format(self._fritzbox)
+        self.fetch_url(url, headers=headers, body=body, method='POST')
+
 
 if __name__ == '__main__':
-    sys.exit(main())
+    logging.basicConfig(level=logging.DEBUG)
+    myplugin = FritzBox('smarthome-dummy', '192.168.2.1')
