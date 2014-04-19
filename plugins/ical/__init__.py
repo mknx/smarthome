615b13e0d1b6dfd9878f47bb6f7cde852f0dec6b
marcus <marcus@popp.mx>
2014-04-19 14:10:57 +0000

    ical: adding timeout keyword

--- plugins/ical/__init__.py
+++ plugins/ical/__init__.py
@@ -52,9 +52,9 @@ class iCal():
     def update_item(self, item, caller=None, source=None, dest=None):
         pass
 
-    def __call__(self, ics, delta=1, offset=0, username=None, password=None):
+    def __call__(self, ics, delta=1, offset=0, username=None, password=None, timeout=2):
         if ics.startswith('http'):
-            ical = self._sh.tools.fetch_url(ics, username=username, password=password)
+            ical = self._sh.tools.fetch_url(ics, username=username, password=password, timeout=timeout)
             if ical is False:
                 return {}
             ical = ical.decode()
