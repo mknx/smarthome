6cbc160c2fe2820c492ad82cae4477a0a9f11c0e
Lars Bernau <L.Bernau@db-central.com>
2014-05-14 14:28:57 +0200

    - changed function names - added possibility to override default device for one call
    - added documentation to  readme.md

--- plugins/pushbullet/__init__.py
+++ plugins/pushbullet/__init__.py
@@ -20,11 +20,11 @@
 #########################################################################
 
 import logging
+
 import json
 import requests
 
 logger = logging.getLogger("Pushbullet")
-
 
 class Pushbullet():
     _apiurl = "https://api.pushbullet.com/api/pushes"
@@ -40,50 +40,47 @@ class Pushbullet():
     def stop(self):
         pass
 
-    def push_note(self, title, body, apikey=None):
-        self._push({"type": "note", "title": title, "body": body}, apikey)
+    def note(self, title, body, deviceid=None, apikey=None):
+        self._push(data={"type": "note", "title": title, "body": body}, deviceid=deviceid, apikey=apikey)
 
-    def push_link(self, title, url, apikey=None):
-        self._push({"type": "link", "url": url, "body": body}, apikey)
+    def link(self, title, url, apikey=None):
+        self._push(data={"type": "link", "url": url, "body": body}, deviceid=deviceid, apikey=apikey)
 
-    def push_address(self, name, address, apikey=None):
-        self._push({"type": "address", "name": name, "address": address}, apikey)
+    def address(self, name, address, apikey=None):
+        self._push(data={"type": "address", "name": name, "address": address}, deviceid=deviceid, apikey=apikey)
 
-    def push_list(self, title, items, apikey=None):
-        self._push({"type": "list", "title": title, "items": items}, apikey)
+    def list(self, title, items, apikey=None):
+        self._push(data={"type": "list", "title": title, "items": items}, deviceid=deviceid, apikey=apikey)
 
-    def push_file(self, filepath, apikey=None):
-        self._push({"type": "file"}, apikey, {"file": open(filepath, "rb")})
+    def file(self, filepath, apikey=None):
+        self._push(data={"type": "file"}, file={"file": open(filepath, "rb")}, deviceid=deviceid, apikey=apikey)
 
-    def _push(self, data, apikey=None, file={}):
+    def _push(self, data, deviceid=deviceid, apikey=None, file={}):
         data["device_iden"] = self._deviceid
+        if deviceid:
+            data["device_iden"] = deviceid
+        
         headers = {"User-Agent": "SmartHome.py"}
         if not file:
             headers["Content-Type"] = "application/json"
+        
         try:
-            resp = requests.post(self._apiurl, data=json.dumps(data), headers=headers, files=file, auth=(self._apikey, ""))
-            if (resp.status_code == 200):
+            response = requests.post(self._apiurl, data=json.dumps(data), headers=headers, files=file, auth=(self._apikey, ""))
+            if (response.status_code == 200):
                 logger.debug("Pushbullet returns: Notification submitted.")
-            elif (resp.status_code == 400):
+            elif (response.status_code == 400):
                 logger.warning("Pushbullet returns: Bad Request - Often missing a required parameter.")
-                logger.warning("Response text: {0}".format(resp.text))
-            elif (resp.status_code == 401):
+            elif (response.status_code == 401):
                 logger.warning("Pushbullet returns: Unauthorized - No valid API key provided.")
-                logger.warning("Response text: {0}".format(resp.text))
-            elif (resp.status_code == 402):
+            elif (response.status_code == 402):
                 logger.warning("Pushbullet returns: Request Failed - Parameters were valid but the request failed.")
-                logger.warning("Response text: {0}".format(resp.text))
-            elif (resp.status_code == 403):
+            elif (response.status_code == 403):
                 logger.warning("Pushbullet returns: Forbidden - The API key is not valid for that request.")
-                logger.warning("Response text: {0}".format(resp.text))
-            elif (resp.status_code == 404):
+            elif (response.status_code == 404):
                 logger.warning("Pushbullet returns: Not Found - The requested item doesn't exist.")
-                logger.warning("Response text: {0}".format(resp.text))
-            elif (resp.status_code >= 500):
+            elif (response.status_code >= 500):
                 logger.warning("Pushbullet returns: Server errors - something went wrong on PushBullet's side.")
-                logger.warning("Response text: {0}".format(resp.text))
             else:
-                logger.error("Pushbullet returns unknown HTTP status code = {0}".format(resp.status_code))
-                logger.warning("Response text: {0}".format(resp.text))
+                logger.error("Pushbullet returns unknown HTTP status code = {0}".format(response.status_code))
         except Exception as e:
             logger.warning("Could not send Pushbullet notification. Error: {0}".format(e))
