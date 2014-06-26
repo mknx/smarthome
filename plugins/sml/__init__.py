a73648b8604c2e2a47e82f6ed1aab761e78e421b
Oliver Hinckel <info@ollisnet.de>
2014-06-26 23:08:13 +0200

    Revert "- remove cycle update and instead of them try continuous reading from device"
    This reverts commit 20c366b5cfbbab647d22d61be0ce8456d4b95939.
    
    Conflicts:
    	plugins/sml/__init__.py

--- plugins/sml/__init__.py
+++ plugins/sml/__init__.py
@@ -45,11 +45,12 @@ class Sml():
       'smart-meter-gateway-com-1' : 'hex'
     }
 
-    def __init__(self, smarthome, host=None, port=0, serialport=None, device="raw"):
+    def __init__(self, smarthome, host=None, port=0, serialport=None, device="raw", cycle=300):
         self._sh = smarthome
         self.host = host
         self.port = int(port)
         self.serialport = serialport
+        self.cycle = cycle
         self.connected = False
         self._serial = None
         self._sock = None
@@ -69,40 +70,15 @@ class Sml():
             logger.warning("Device type \"{}\" not supported - defaulting to \"raw\"".format(device))
             self._prepare = self._prepareRaw
 
+        smarthome.connections.monitor(self)
+
     def run(self):
         self.alive = True
-
-        while self.alive:
-            self.connect()
-
-            while not self.connected:
-                logger.debug('Not connected - waiting 5 seconds ...')
-                time.sleep(5)
-
-            while self.alive and self.connected:
-                try:
-                    data = self._read(512)
-
-                    if len(data):
-                        values = self._parse(self._prepare(data))
-
-                        for obis in values:
-                            logger.debug('Entry {}'.format(values[obis]))
-
-                            if obis in self._items:
-                                for prop in self._items[obis]:
-                                    for item in self._items[obis][prop]:
-                                        item(values[obis][prop], 'Sml')
-                    else:
-                        time.sleep(1)
-
-                except Exception as e:
-                    logger.error('Reading data from {0} failed: {1}'.format(self._target, e))
-
-        self.disconnect()
+        self._sh.scheduler.add('Sml', self._refresh, cycle=self.cycle)
 
     def stop(self):
         self.alive = False
+        self.disconnect()
 
     def parse_item(self, item):
         if 'sml_obis' in item.conf:
@@ -125,7 +101,7 @@ class Sml():
 
     def connect(self):
         self._lock.acquire()
-        self._target = None
+        target = None
         try:
             if self.serialport is not None:
                 self._target = 'serial://{}'.format(self.serialport)
@@ -183,6 +159,29 @@ class Sml():
 
         return b''.join(total)
         
+    def _refresh(self):
+        if self.connected:
+            start = time.time()
+            try:
+                data = self._read(512)
+
+                values = self._parse(self._prepare(data))
+
+                for obis in values:
+                    logger.debug('Entry {}'.format(values[obis]))
+
+                    if obis in self._items:
+                        for prop in self._items[obis]:
+                            for item in self._items[obis][prop]:
+                                item(values[obis][prop], 'Sml')
+
+            except Exception as e:
+                logger.error('Reading data from {0} failed: {1}'.format(self._target, e))
+
+
+            cycletime = time.time() - start
+            logger.debug("cycle takes {0} seconds".format(cycletime))
+
     def _parse(self, data):
         # Search SML List Entry sequences like:
         # "77 07 81 81 c7 82 03 ff 01 01 01 01 04 xx xx xx xx" - manufactor
